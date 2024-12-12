351ba9dd59a6b6d08a09cfde77f1561a89841f1c
MartinGonzalezNovoa <martinpedro.gonzalez@gmail.com>
2024-12-12 11:21:51 -0300

    Add files via upload

---
+++ docs/animaciones.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const videoElement = document.querySelector("#como-se-llego-a-esto iframe");
+
+    function handleScroll() {
+        const videoPosition = videoElement.getBoundingClientRect();
+        const viewportHeight = window.innerHeight;
+
+        // Detecta si el video está en la vista
+        if (videoPosition.top >= 0 && videoPosition.bottom <= viewportHeight) {
+            const iframeSrc = videoElement.src;
+            if (!iframeSrc.includes("autoplay=1")) {
+                videoElement.src = iframeSrc + (iframeSrc.includes("?") ? "&autoplay=1" : "?autoplay=1");
+            }
+            window.removeEventListener("scroll", handleScroll); // Elimina el evento después de reproducirlo
+        }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+});
